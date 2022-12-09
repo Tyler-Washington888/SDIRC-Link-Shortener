f8044eeeb0c12d4928639b9adc34da4b2d8cbe45
Washington <lilturtleneckbeats.gmail.com>
2022-12-09 15:07:59 -0500

    complete services file

--- client/src/services/links.js
+++ client/src/services/links.js
@@ -1,10 +1,17 @@
 import api from './apiConfig'
 
-// /api/links
-
 export const getLinks = async () => {
     try {
       const response = await api.get('/api/links/')
+      return response.data
+    } catch (error) {
+      throw error
+    }
+}
+
+export const getMyLinks = async (email) => {
+    try {
+      const response = await api.get('/api/links/myLinks', email)
       return response.data
     } catch (error) {
       throw error
@@ -22,7 +29,16 @@ export const createPost = async (post) => {
 
 export const updatePost = async (id, post) => {
     try {
-        const response = await api.post('/api/links/', post)
+        const response = await api.put(`/api/links/${id}`, post)
+        return response.data
+    } catch (error) {
+        throw error
+    }
+}
+
+export const deletePost = async (id) => {
+    try {
+        const response = await api.delete(`/api/links/${id}`)
         return response.data
     } catch (error) {
         throw error
