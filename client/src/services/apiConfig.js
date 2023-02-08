37c022a2e1a37418a485afc4f7c454f430245095
Washington <lilturtleneckbeats.gmail.com>
2023-02-08 08:02:33 -0500

    changed prodcution variable

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,7 +1,10 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'http://localhost:5000' : 'http://localhost:5000'
-})
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://sdirc-backend-deploy.herokuapp.com/"
+      : "http://localhost:5000",
+});
 
 export default api;
